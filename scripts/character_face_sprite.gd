7c816510af2e2965a03eac6678e14d99f6ed794e
NAERNON <naernon@icloud.com>
2026-01-31 12:09:22 +0100

    AnimationGraph added

--- scripts/character_face_sprite.gd
+++ scripts/character_face_sprite.gd
@@ -1,39 +1,31 @@
-class_name CharacterFaceSprite extends Node2D
-
-@onready var _sprite: AnimatedSprite2D = AnimatedSprite2D.new()
-@onready var _timer: Timer = Timer.new()
-@onready var _rng: RandomNumberGenerator = RandomNumberGenerator.new()
-
-var _state: State = State.IDLE;
+class_name CharacterFaceSprite extends AnimationGraph
 
 enum State {
-	IDLE
+	IDLE,
+	IDLE_ANIM,
 }
 
 func _ready() -> void:
-	var sprite_frames: SpriteFrames = load("uid://bwh5qaed5uy1u")
-	_sprite.sprite_frames = sprite_frames
-	_sprite.play("default")
-	add_child(_sprite)
-	
-	_timer.one_shot = true;
-	_timer.timeout.connect(_on_timer_timeout)
-	add_child(_timer)
-	
+	super()
 	set_state(State.IDLE)
 
-func _on_timer_timeout() -> void:
-	match _state:
+func sprite_frames() -> SpriteFrames:
+	return load("uid://bwh5qaed5uy1u")
+
+func animation_to_play_for_state(state: int) -> String:
+	match state:
+		State.IDLE: return "default"
+		State.IDLE_ANIM: return "idle%03d" % randi_range(1,4)
+	return String()
+
+func transition_after_state(state: int) -> AnimationGraphTransition:
+	match state:
 		State.IDLE:
-			_sprite.play("idle001")
-			_start_idle_animation_timer()
-
-func _start_idle_animation_timer() -> void:
-	_timer.stop()
-	_timer.wait_time = _rng.randf_range(1.0, 3.0)
-	_timer.start()
-
-func set_state(state: State) -> void:
-	_state = state
-	_sprite.play("default")
-	_start_idle_animation_timer()
+			return AnimationGraphTransition.random(
+				State.IDLE_ANIM, 1.0, 3.0
+			)
+		State.IDLE_ANIM:
+			return AnimationGraphTransition.random(
+				State.IDLE_ANIM, 1.0, 5.0
+			)
+	return null
