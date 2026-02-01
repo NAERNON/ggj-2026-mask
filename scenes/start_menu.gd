dfac8d1f494f6d5f69459cb0502ac436a3fc8f4b
NAERNON <naernon@icloud.com>
2026-02-01 15:48:32 +0100

    Start menu

--- scenes/start_menu.gd
+++ scenes/start_menu.gd
@@ -1,11 +1,13 @@
 class_name StartMenu extends Node2D
 
 @export var play_button: ScotchMenuButton
+@export var tape_wheel: MaskingTapeWheel
 
 signal start_game()
 
 func _ready() -> void:
 	play_button.highlight()
+	tape_wheel.max_weight = 0.0
 
 func _input(event: InputEvent) -> void:
 	if visible and event.is_action_pressed("ui_accept"):
